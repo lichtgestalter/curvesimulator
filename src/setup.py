f3873c93762449ea93b131ffc3f155591124925b
lichtgestalter <uli.scheuss@gmail.com>
2024-11-11 14:04:57 +0100

    0.2.3

--- src/setup.py
+++ src/setup.py
@@ -1,5 +1,8 @@
 from setuptools import setup, find_packages
 
+with open("../README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name="curvesimulator",
     version="0.2.3",
