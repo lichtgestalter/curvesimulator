8b59f1d764a1ae61a9898f965a7db4cb03acceba
lichtgestalter <uli.scheuss@gmail.com>
2024-12-08 00:25:37 +0100

    0.2.11 Default inclination 90 iso 0 degrees

--- src/setup.py
+++ src/setup.py
@@ -13,7 +13,7 @@ with open(readme_path, "r", encoding="utf-8") as fh:
 
 setup(
     name="curvesimulator",
-    version="0.2.10",
+    version="0.2.11",
     packages=find_packages(),
     install_requires=[
         # List your dependencies here
