da0cb098be80ea794faee42d39d664c7dadbab4b
lichtgestalter <uli.scheuss@gmail.com>
2024-12-24 17:01:59 +0100

    0.2.15 New Projection

--- src/try_curvesim.py
+++ src/try_curvesim.py
@@ -25,6 +25,9 @@ def main():
     print(parameters)
     print(bodies)
     print(lightcurve)
+    # for i in range(0, len(bodies[1].positions), 182):
+    #     print(f"{i:4}: z:{bodies[1].positions[i][2]:12.0f}  x:{bodies[1].positions[i][0]:12.0f}")
+
 
 if __name__ == '__main__':
     main()
