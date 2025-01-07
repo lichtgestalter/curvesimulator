d41cc465fa48a1b34ff38261bb76671f66e071f7
lichtgestalter <uli.scheuss@gmail.com>
2025-01-07 12:01:09 +0100

    0.2.22 Example Videos TOI-4504

--- minitest.py
+++ minitest.py
@@ -1,22 +1,56 @@
+import matplotlib.pyplot as plt
+import numpy as np
 import math
 
+def limbdarkening(relative_radius, limb_darkening_coefficients):
+    if relative_radius < 0:  # handling rounding errors
+        relative_radius = 0.0												
+    if relative_radius > 1:												
+        relative_radius = 1.0												
+    mu = math.sqrt(1 - relative_radius ** 2)  # mu = cos(theta), where theta is the angle from the center												
+    # intensity = sum(a * mu ** i for i, a in enumerate(limb_darkening_coefficients))
+    intensity = 0
+    for i, a in enumerate(limb_darkening_coefficients):
+        intensity += a * mu ** i
+    return intensity												
 
-def calculate_gravitational_parameter(Am, Bm, P):
-    # Gravitational constant (m^3 kg^-1 s^-2)
-    G = 6.67430e-11
-    sm = 1.98847e30
-    # Convert masses to kg if given in solar masses
-    Am_kg = Am * sm
-    Bm_kg = Bm * sm
-    P_seconds = P * 24 * 3600
-    mu = G * (Am_kg + Bm_kg)
-    return mu
+def mean_intensity(limb_darkening_coefficients):
+    """Calculates the ratio of the mean intensity to the central intensity of a star based on the given coefficients."""
+    if limb_darkening_coefficients is None:
+        return None
+    intensity = 0.0
+    for i, c in enumerate(limb_darkening_coefficients):
+        intensity += 2.0 * c / (i + 2)
+    return intensity
+
+def mean_intensity_uli(limb_darkening_coefficients, n):
+    """Calculates the ratio of the mean intensity to the central intensity of a star based on the given coefficients."""
+    radii = np.linspace(0, 1, n)
+    luminosity = 0
+    for r_min, r_max in zip(radii[:-1], radii[1:]):
+        area = math.pi * (r_max**2 - r_min**2)
+        r_middle = (r_min + r_max) / 2
+        brightness = limbdarkening(r_middle, limb_darkening_coefficients)
+        luminosity += brightness * area
+    return luminosity/math.pi
 
 
-# Example usage
-Am = 6.5  # Mass of star A in solar masses
-Bm = 5.9  # Mass of star B in solar masses
-P = 1.1047  # Orbital period in days
+# ldc = [0.4765, 0.3495, 0.174]
+ldc = [0.3495, 0.4765]
+# ldc = [0.3, 0.9, -0.2]
+mean = mean_intensity(ldc)
+print("def", mean_intensity(ldc))
+print("uli", mean_intensity_uli(ldc, 1000))
+exit(1)
+# plot the limb darkening curve for a given set of coefficients and radii between 0 and 1
+radii = np.linspace(0, 1, 100)
+intensities = [limbdarkening(r, ldc) for r in radii]
+plt.plot(radii, intensities)
+plt.xlabel('Normalized Radius')
+plt.ylabel('Relative Intensity')
+plt.title('Limb Darkening Curve')
+#shwo the mean intensity as a horizontal line
+plt.axhline(y=mean, color='r', linestyle='--', label='Mean Intensity')
+plt.show()
 
-mu = calculate_gravitational_parameter(Am, Bm, P)
-print(f"Gravitational parameter: {mu:.2e} m^3/s^2")
+
