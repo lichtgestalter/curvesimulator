64fb70025678eeaef8c6c8fb80977bb2623527a5
lichtgestalter <uli.scheuss@gmail.com>
2025-01-18 23:16:03 +0100

    0.3.2 Result output

--- src/curvesimulator/cs_body.py
+++ src/curvesimulator/cs_body.py
@@ -211,53 +211,57 @@ class CurveSimBody:
             self.positions[0] = np.array(pos, dtype=float)  # [m] initial position
             self.velocity = np.array(vel, dtype=float)  # [m/s] initial velocity
 
+    def full_eclipse(self, other, d):
+        if self.radius < other.radius:  # Total eclipse
+            area = self.area_2d
+            relative_radius = 0
+            # print(f'  total: {iteration:7d}  rel.area: {area/self.area_2d*100:6.0f}%  rel.r: {relative_radius*100:6.0f}%')
+            return area, relative_radius
+        else:  # Annular (i.e. ring) eclipse
+            area = other.area_2d
+            relative_radius = d / self.radius
+            # if debugging_eclipse and iteration % 1 == 0:
+            #     print(f'ring eclipse i:{iteration:5d}  ecl.area: {area/self.area_2d*100:4.1f}%  rel.r: {relative_radius*100:4.1f}%', end="  ")
+            #     print(f"dy: {abs(self.positions[iteration][1]-other.positions[iteration][1]):6.3e}  dz: {abs(self.positions[iteration][2]-other.positions[iteration][2]):6.3e} d: {d:6.3e}")
+            # print(f'   ring: {iteration:7d}  rel.area: {area / self.area_2d * 100:6.0f}%  rel.r: {relative_radius * 100:6.0f}%')
+            return area, relative_radius
+
+    def partial_eclipse(self, other, d):
+        self.d = (self.radius ** 2 - other.radius ** 2 + d ** 2) / (2 * d)  # Distance of center from self to radical axis
+        other.d = (other.radius ** 2 - self.radius ** 2 + d ** 2) / (2 * d)  # Distance of center from other to radical axis
+        other.h = other.radius + self.d - d  # Height of circle segment
+        self.h = self.radius + other.d - d  # Height of circle segment
+        other.angle = 2 * math.acos(1 - other.h / other.radius)  # Angle of circle segment
+        self.angle = 2 * math.acos(1 - self.h / self.radius)  # Angle of circle segment
+        other.eclipsed_area = other.radius ** 2 * (other.angle - math.sin(other.angle)) / 2  # Area of circle segment
+        self.eclipsed_area = self.radius ** 2 * (self.angle - math.sin(self.angle)) / 2  # Area of circle segment
+        area = other.eclipsed_area + self.eclipsed_area  # Eclipsed area is sum of two circle segments.
+        relative_radius = (self.radius + self.d - other.h) / (2 * self.radius)  # Relative distance between approximated center C of eclipsed area and center of self
+        # if debugging_eclipse and iteration % 1 == 0:
+        #     print(f'partial eclipse i:{iteration:5d}  ecl.area: {area / self.area_2d * 100:4.1f}%  rel.r: {relative_radius * 100:4.1f}%', end="  ")
+        #     print(f"dy: {abs(self.positions[iteration][1] - other.positions[iteration][1]):6.3e}  dz: {abs(self.positions[iteration][2] - other.positions[iteration][2]):6.3e} d: {d:6.3e}")
+        return area, relative_radius
+
     def eclipsed_by(self, other, iteration, results):
         """Returns area, relative_radius
         area: Area of self which is eclipsed by other.
         relative_radius: The distance of the approximated center of the eclipsed area from the center of self as a percentage of self.radius (used for limb darkening)."""
         if other.positions[iteration][2] > self.positions[iteration][2]:  # Is other nearer to viewpoint than self? (i.e. its position has a larger z-coordinate)
-            # print(other.name, 'is nearer than', self.name)
             d = CurveSimPhysics.distance_2d_ecl(other, self, iteration)
-            # print(f'{self.name} {other.name} {d=}')
             if d < self.radius + other.radius:  # Does other eclipse self?
                 if d <= abs(self.radius - other.radius):  # Annular (i.e. ring) eclipse or total eclipse
                     if other.eclipsing.value < CurveSimBody.Eclipsing.MAX.value:  # is this T2?
                         other.eclipsing = CurveSimBody.Eclipsing.MAX
-                        # results[other.name] =
+                        results[other.name] = hier weiter
                         # print(f"\n{iteration=} {other.name} eclipses {self.name} maximally.")
-                    if self.radius < other.radius:  # Total eclipse
-                        area = self.area_2d
-                        relative_radius = 0
-                        # print(f'  total: {iteration:7d}  rel.area: {area/self.area_2d*100:6.0f}%  rel.r: {relative_radius*100:6.0f}%')
-                        return area, relative_radius
-                    else:  # Annular (i.e. ring) eclipse
-                        area = other.area_2d
-                        relative_radius = d / self.radius
-                        if debugging_eclipse and iteration % 1 == 0:
-                            print(f'ring eclipse i:{iteration:5d}  ecl.area: {area/self.area_2d*100:4.1f}%  rel.r: {relative_radius*100:4.1f}%', end="  ")
-                            print(f"dy: {abs(self.positions[iteration][1]-other.positions[iteration][1]):6.3e}  dz: {abs(self.positions[iteration][2]-other.positions[iteration][2]):6.3e} d: {d:6.3e}")
-                            # print(f'   ring: {iteration:7d}  rel.area: {area / self.area_2d * 100:6.0f}%  rel.r: {relative_radius * 100:6.0f}%')
-                        return area, relative_radius
+                    return self.full_eclipse(other, d)
                 else:  # Partial eclipse
                     if other.eclipsing.value != CurveSimBody.Eclipsing.PARTIAL.value:  # is this T1 or T3?
                         other.eclipsing = CurveSimBody.Eclipsing.PARTIAL
                         # print(f"\n{iteration=} {other.name} eclipses {self.name} partially.")
                     # Eclipsed area is the sum of a circle segment of self + a circle segment of other
                     # https://de.wikipedia.org/wiki/Kreissegment  https://de.wikipedia.org/wiki/Schnittpunkt#Schnittpunkte_zweier_Kreise
-                    self.d = (self.radius ** 2 - other.radius ** 2 + d ** 2) / (2 * d)  # Distance of center from self to radical axis
-                    other.d = (other.radius ** 2 - self.radius ** 2 + d ** 2) / (2 * d)  # Distance of center from other to radical axis
-                    other.h = other.radius + self.d - d  # Height of circle segment
-                    self.h = self.radius + other.d - d  # Height of circle segment
-                    other.angle = 2 * math.acos(1 - other.h / other.radius)  # Angle of circle segment
-                    self.angle = 2 * math.acos(1 - self.h / self.radius)  # Angle of circle segment
-                    other.eclipsed_area = other.radius ** 2 * (other.angle - math.sin(other.angle)) / 2  # Area of circle segment
-                    self.eclipsed_area = self.radius ** 2 * (self.angle - math.sin(self.angle)) / 2  # Area of circle segment
-                    area = other.eclipsed_area + self.eclipsed_area  # Eclipsed area is sum of two circle segments.
-                    relative_radius = (self.radius + self.d - other.h) / (2 * self.radius)  # Relative distance between approximated center C of eclipsed area and center of self
-                    if debugging_eclipse and iteration % 1 == 0:
-                        print(f'partial eclipse i:{iteration:5d}  ecl.area: {area / self.area_2d * 100:4.1f}%  rel.r: {relative_radius * 100:4.1f}%', end="  ")
-                        print(f"dy: {abs(self.positions[iteration][1] - other.positions[iteration][1]):6.3e}  dz: {abs(self.positions[iteration][2] - other.positions[iteration][2]):6.3e} d: {d:6.3e}")
-                    return area, relative_radius
+                    return self.partial_eclipse(other, d)
             else:  # No eclipse because, seen from viewer, the bodies are not close enough to each other
                 if other.eclipsing.value > CurveSimBody.Eclipsing.NO.value:  # is this T4?
                     other.eclipsing = CurveSimBody.Eclipsing.NO
