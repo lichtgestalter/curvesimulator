1b5c33fe651b9959009b5e668e038a51435ca37f
lichtgestalter <uli.scheuss@gmail.com>
2025-02-19 23:47:43 +0100

    0.3.6 state_vector_to_keplerian_elements

--- debug/lightkurve_test.py
+++ debug/lightkurve_test.py
@@ -1,3 +1,12 @@
 # https://lightkurve.github.io/lightkurve/tutorials/3-science-examples/exoplanets-identifying-transiting-planet-signals.html
 
-import lightkurve
+from lightkurve import TessLightCurve
+from matplotlib import pyplot as plt
+
+sectors = ["27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "61", "62", "63", "64", "65", "67", "68", "69"]
+for sector in sectors:
+    file = f"../research/star_systems/TOI-4504/lightkurve/{sector}/{sector}.fits"
+    lc = TessLightCurve.read(file)
+    lc.plot()
+    plt.savefig(f"../research/star_systems/TOI-4504/lightkurve/{sector}.png")
+    plt.close()  # Close the plot to free up memory
