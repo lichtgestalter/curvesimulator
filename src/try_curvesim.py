51ce537ea3cfae7b489071e0ca06a800ee7f6c02
lichtgestalter <uli.scheuss@gmail.com>
2025-02-24 01:16:45 +0100

    0.3.6 state_vector_to_keplerian_elements

--- src/try_curvesim.py
+++ src/try_curvesim.py
@@ -1,13 +1,13 @@
 # Short version:
-# from curvesimulator.curvesim import curvesim
-# curvesim("MyConfigFileName.ini")
+from curvesimulator import curvesim
+curvesim("../configurations/MyFirstConfigFile.ini")
 #
 #
 # Long version:
 # from curvesimulator import curvesim
 #
 # def main():
-#     parameters, bodies, lightcurve = curvesim(config_file="../configurations/MyFirstConfigFile.ini")
+#     parameters, bodies, results, lightcurve = curvesim(config_file="../configurations/MyFirstConfigFile.ini")
 #     print(parameters)
 #     print(bodies)
 #     print(results)
@@ -17,17 +17,17 @@
 # if __name__ == '__main__':
 #     main()
 
-from curvesimulator import curvesim
-
-def main():
-    # parameters, bodies = debug_print_points()
-    # parameters, bodies, results, lightcurve = curvesim(config_file="../configurations/TIC470710327.ini")
-    parameters, bodies, results, lightcurve = curvesim(config_file="../configurations/TOI-4504.ini")
-    # print(parameters)
-    print(bodies)
-    print(results)
-    print(lightcurve)
-
-
-if __name__ == '__main__':
-    main()
+# from curvesimulator import curvesim
+#
+# def main():
+#     # parameters, bodies = debug_print_points()
+#     # parameters, bodies, results, lightcurve = curvesim(config_file="../configurations/TIC470710327.ini")
+#     parameters, bodies, results, lightcurve = curvesim(config_file="../configurations/TOI-4504.ini")
+#     # print(parameters)
+#     print(bodies)
+#     print(results)
+#     print(lightcurve)
+#
+#
+# if __name__ == '__main__':
+#     main()
