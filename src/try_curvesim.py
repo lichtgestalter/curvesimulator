a090fa38c4136f5f0ed66145e2af98038dc60a90
lichtgestalter <uli.scheuss@gmail.com>
2025-02-24 15:06:19 +0100

    0.3.5 Result file usage examples

--- src/try_curvesim.py
+++ src/try_curvesim.py
@@ -1,6 +1,6 @@
 # Short version:
-from curvesimulator import curvesim
-curvesim("../configurations/MyFirstConfigFile.ini")
+# from curvesimulator import curvesim
+# curvesim("../configurations/MyFirstConfigFile.ini")
 #
 #
 # Long version:
@@ -17,17 +17,17 @@ curvesim("../configurations/MyFirstConfigFile.ini")
 # if __name__ == '__main__':
 #     main()
 
-# from curvesimulator import curvesim
-#
-# def main():
-#     # parameters, bodies = debug_print_points()
-#     # parameters, bodies, results, lightcurve = curvesim(config_file="../configurations/TIC470710327.ini")
-#     parameters, bodies, results, lightcurve = curvesim(config_file="../configurations/TOI-4504.ini")
-#     # print(parameters)
-#     print(bodies)
-#     print(results)
-#     print(lightcurve)
-#
-#
-# if __name__ == '__main__':
-#     main()
+from curvesimulator import curvesim
+
+def main():
+    # parameters, bodies = debug_print_points()
+    # parameters, bodies, results, lightcurve = curvesim(config_file="../configurations/TIC470710327.ini")
+    parameters, bodies, results, lightcurve = curvesim(config_file="../configurations/TOI-4504.ini")
+    # print(parameters)
+    print(bodies)
+    print(results)
+    print(lightcurve)
+
+
+if __name__ == '__main__':
+    main()
