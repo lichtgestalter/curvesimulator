c1ab5f2226f8369f7212607f32ebdd78cbe34e50
lichtgestalter <uli.scheuss@gmail.com>
2025-04-27 23:08:48 +0200

    0.4.3   Testing occultquad.py

--- evaluation/SimonsCode/occultquad.py
+++ evaluation/SimonsCode/occultquad.py
@@ -1,12 +1,11 @@
-from numpy import size, zeros, where, arccos, sqrt, pi, log
-from numpy import asarray  # Uli
+import numpy as np
 
 
 # Computes Hasting's polynomial approximation for the complete
 # elliptic integral of the first (ek) and second (kk) kind
 def ellke(k):
     m1 = 1. - k ** 2
-    logm1 = log(m1)
+    logm1 = np.log(m1)
 
     a1 = 0.44325141463
     a2 = 0.06260601220
@@ -40,13 +39,13 @@ def ellke(k):
 # Computes the complete elliptical integral of the third kind using
 # the algorithm of Bulirsch (1965):
 def ellpic_bulirsch(n, k):
-    kc = sqrt(1. - k ** 2)
+    kc = np.sqrt(1. - k ** 2)
     p = n + 1.
     if min(p) < 0.:
         print('Negative p')
     m0 = 1.
     c = 1.
-    p = sqrt(p)
+    p = np.sqrt(p)
     d = 1. / p
     e = kc
     while 1:
@@ -58,10 +57,10 @@ def ellpic_bulirsch(n, k):
         g = m0
         m0 = kc + m0
         if max(abs(1. - kc / g)) > 1.e-8:
-            kc = 2 * sqrt(e)
+            kc = 2 * np.sqrt(e)
             e = kc * m0
         else:
-            return 0.5 * pi * (c * m0 + d) / (m0 * (m0 + p))
+            return 0.5 * np.pi * (c * m0 + d) / (m0 * (m0 + p))
 
 
 #   Python translation of IDL code.
@@ -73,15 +72,28 @@ def ellpic_bulirsch(n, k):
 def occultquad(z, u1, u2, p0):
     """
     See https://arxiv.org/abs/astro-ph/0210099 (Analytic Lightcurves for Planetary Transit Searches)
-    z: Distance between the centers of the bodies (star and planet) in units of stellar radii
-    u1, u2: Quadratic limb darkening coefficients
-    p0: Planet radius in units of stellar radii
+    Parameters:
+    z: <numpy array> or <list of floats> distance between the centers of the star and planet in units of stellar radii
+    u1: <float> first quadratic limb-darkening coefficient
+    u2: <float> second quadratic limb-darkening coefficient
+    p0: <float> planet radius in units of stellar radii
     ???: The measured brightness (flux) of the star system outside of transit
+    Return values:
+    muo1: Normalized flux of the star system, accounting for limb-darkening during transit.
+    mu0: Normalized flux of the star system without considering limb darkening.
+
+
+Both values are normalized to the out-of-transit flux of the star system, meaning they are expressed as fractions of the star's brightness when no planet is transiting.
     """
-    nz = size(z)
-    lambdad = zeros(nz)
-    etad = zeros(nz)
-    lambdae = zeros(nz)
+
+    nz = np.size(z)
+    lambdad = np.zeros(nz)
+    uli = np.zeros(nz)
+    uli1 = np.zeros(nz)
+    uli2 = np.zeros(nz)
+    uli3 = np.zeros(nz)
+    etad = np.zeros(nz)
+    lambdae = np.zeros(nz)
     omega = 1. - u1 / 3. - u2 / 6.
 
     # tolerance for double precision equalities
@@ -90,10 +102,10 @@ def occultquad(z, u1, u2, p0):
 
     p = abs(p0)
 
-    z = where(abs(p - z) < tol, p, z)
-    z = where(abs((p - 1) - z) < tol, p - 1., z)
-    z = where(abs((1 - p) - z) < tol, 1. - p, z)
-    z = where(z < tol, 0., z)
+    z = np.where(abs(p - z) < tol, p, z)
+    z = np.where(abs((p - 1) - z) < tol, p - 1., z)
+    z = np.where(abs((1 - p) - z) < tol, 1. - p, z)
+    z = np.where(z < tol, 0., z)
 
     x1 = (p - z) ** 2.
     x2 = (p + z) ** 2.
@@ -101,17 +113,15 @@ def occultquad(z, u1, u2, p0):
 
     # trivial case of no planet
     if p <= 0.:
-        muo1 = zeros(nz) + 1.
-        mu0 = zeros(nz) + 1.
+        muo1 = np.zeros(nz) + 1.
+        mu0 = np.zeros(nz) + 1.
         return [muo1, mu0]
 
     # Case 1 - the star is unocculted:
     # only consider points with z lt 1+p
-    notusedyet = where(z < (1. + p))
-    # notusedyet = asarray(z < (1. + p)).nonzero()   # Uli
-    # notusedyet = asarray(z < (1. + p))   # Uli
+    notusedyet = np.where(z < (1. + p))
     notusedyet = notusedyet[0]
-    if size(notusedyet) == 0:
+    if np.size(notusedyet) == 0:
         muo1 = 1. - ((1. - u1 - 2. * u2) * lambdae + (u1 + 2. * u2) * (lambdad + 2. / 3. * (p > z)) +
                      u2 * etad) / omega
         mu0 = 1. - lambdae
@@ -119,14 +129,14 @@ def occultquad(z, u1, u2, p0):
 
     # Case 11 - the  source is completely occulted:
     if p >= 1.:
-        occulted = where(z[notusedyet] <= p - 1.)  #,complement=notused2)
-        if size(occulted) != 0:
+        occulted = np.where(z[notusedyet] <= p - 1.)  #,complement=notused2)
+        if np.size(occulted) != 0:
             ndxuse = notusedyet[occulted]
             etad[ndxuse] = 0.5  # corrected typo in paper
             lambdae[ndxuse] = 1.
             # lambdad = 0 already
-            notused2 = where(z[notusedyet] > p - 1)
-            if size(notused2) == 0:
+            notused2 = np.where(z[notusedyet] > p - 1)
+            if np.size(notused2) == 0:
                 muo1 = 1. - ((1. - u1 - 2. * u2) * lambdae + (u1 + 2. * u2) * (lambdad + 2. / 3. *
                                                                                (p > z)) + u2 * etad) / omega
                 mu0 = 1. - lambdae
@@ -134,36 +144,36 @@ def occultquad(z, u1, u2, p0):
             notusedyet = notusedyet[notused2]
 
     # Case 2, 7, 8 - ingress/egress (uniform disk only)
-    inegressuni = where((z[notusedyet] >= abs(1. - p)) & (z[notusedyet] < 1. + p))
-    if size(inegressuni) != 0:
+    inegressuni = np.where((z[notusedyet] >= abs(1. - p)) & (z[notusedyet] < 1. + p))
+    if np.size(inegressuni) != 0:
         ndxuse = notusedyet[inegressuni]
         tmp = (1. - p ** 2. + z[ndxuse] ** 2.) / 2. / z[ndxuse]
-        tmp = where(tmp > 1., 1., tmp)
-        tmp = where(tmp < -1., -1., tmp)
-        kap1 = arccos(tmp)
+        tmp = np.where(tmp > 1., 1., tmp)
+        tmp = np.where(tmp < -1., -1., tmp)
+        kap1 = np.arccos(tmp)
         tmp = (p ** 2. + z[ndxuse] ** 2 - 1.) / 2. / p / z[ndxuse]
-        tmp = where(tmp > 1., 1., tmp)
-        tmp = where(tmp < -1., -1., tmp)
-        kap0 = arccos(tmp)
+        tmp = np.where(tmp > 1., 1., tmp)
+        tmp = np.where(tmp < -1., -1., tmp)
+        kap0 = np.arccos(tmp)
         tmp = 4. * z[ndxuse] ** 2 - (1. + z[ndxuse] ** 2 - p ** 2) ** 2
-        tmp = where(tmp < 0, 0, tmp)
-        lambdae[ndxuse] = (p ** 2 * kap0 + kap1 - 0.5 * sqrt(tmp)) / pi
+        tmp = np.where(tmp < 0, 0, tmp)
+        lambdae[ndxuse] = (p ** 2 * kap0 + kap1 - 0.5 * np.sqrt(tmp)) / np.pi
         # eta_1
-        etad[ndxuse] = 1. / 2. / pi * (kap1 + p ** 2 * (p ** 2 + 2. * z[ndxuse] ** 2) * kap0 -
-                                       (1. + 5. * p ** 2 + z[ndxuse] ** 2) / 4. * sqrt((1. - x1[ndxuse]) * (x2[ndxuse] - 1.)))
+        etad[ndxuse] = 1. / 2. / np.pi * (kap1 + p ** 2 * (p ** 2 + 2. * z[ndxuse] ** 2) * kap0 -
+                                          (1. + 5. * p ** 2 + z[ndxuse] ** 2) / 4. * np.sqrt((1. - x1[ndxuse]) * (x2[ndxuse] - 1.)))
 
     # Case 5, 6, 7 - the edge of planet lies at origin of star
-    ocltor = where(z[notusedyet] == p)  #, complement=notused3)
-    t = where(z[notusedyet] == p)
-    if size(ocltor) != 0:
+    ocltor = np.where(z[notusedyet] == p)  #, complement=notused3)
+    t = np.where(z[notusedyet] == p)
+    if np.size(ocltor) != 0:
         ndxuse = notusedyet[ocltor]
         if p < 0.5:
             # Case 5
             q = 2. * p  # corrected typo in paper (2k -> 2p)
             Ek, Kk = ellke(q)
             # lambda_4
-            lambdad[ndxuse] = 1. / 3. + 2. / 9. / pi * (4. * (2. * p ** 2 - 1.) * Ek +
-                                                        (1. - 4. * p ** 2) * Kk)
+            lambdad[ndxuse] = 1. / 3. + 2. / 9. / np.pi * (4. * (2. * p ** 2 - 1.) * Ek +
+                                                           (1. - 4. * p ** 2) * Kk)
             # eta_2
             etad[ndxuse] = p ** 2 / 2. * (p ** 2 + 2. * z[ndxuse] ** 2)
             lambdae[ndxuse] = p ** 2  # uniform disk
@@ -172,15 +182,15 @@ def occultquad(z, u1, u2, p0):
             q = 0.5 / p  # corrected typo in paper (1/2k -> 1/2p)
             Ek, Kk = ellke(q)
             # lambda_3
-            lambdad[ndxuse] = 1. / 3. + 16. * p / 9. / pi * (2. * p ** 2 - 1.) * Ek - \
-                              (32. * p ** 4 - 20. * p ** 2 + 3.) / 9. / pi / p * Kk
+            lambdad[ndxuse] = 1. / 3. + 16. * p / 9. / np.pi * (2. * p ** 2 - 1.) * Ek - \
+                              (32. * p ** 4 - 20. * p ** 2 + 3.) / 9. / np.pi / p * Kk
             # etad = eta_1 already
         else:
             # Case 6
-            lambdad[ndxuse] = 1. / 3. - 4. / pi / 9.
+            lambdad[ndxuse] = 1. / 3. - 4. / np.pi / 9.
             etad[ndxuse] = 3. / 32.
-        notused3 = where(z[notusedyet] != p)
-        if size(notused3) == 0:
+        notused3 = np.where(z[notusedyet] != p)
+        if np.size(notused3) == 0:
             muo1 = 1. - ((1. - u1 - 2. * u2) * lambdae + (u1 + 2. * u2) *
                          (lambdad + 2. / 3. * (p > z)) + u2 * etad) / omega
             mu0 = 1. - lambdae
@@ -188,29 +198,29 @@ def occultquad(z, u1, u2, p0):
         notusedyet = notusedyet[notused3]
 
     # Case 2, Case 8 - ingress/egress (with limb darkening)
-    inegress = where(((z[notusedyet] > 0.5 + abs(p - 0.5)) &
-                      (z[notusedyet] < 1. + p)) | \
-                     ((p > 0.5) & (z[notusedyet] > abs(1. - p)) &
+    inegress = np.where(((z[notusedyet] > 0.5 + abs(p - 0.5)) &
+                         (z[notusedyet] < 1. + p)) | \
+                        ((p > 0.5) & (z[notusedyet] > abs(1. - p)) &
                       (z[notusedyet] < p)))  #, complement=notused4)
-    if size(inegress) != 0:
+    if np.size(inegress) != 0:
 
         ndxuse = notusedyet[inegress]
-        q = sqrt((1. - x1[ndxuse]) / (x2[ndxuse] - x1[ndxuse]))
+        q = np.sqrt((1. - x1[ndxuse]) / (x2[ndxuse] - x1[ndxuse]))
         Ek, Kk = ellke(q)
         n = 1. / x1[ndxuse] - 1.
 
         # lambda_1:
-        lambdad[ndxuse] = 2. / 9. / pi / sqrt(x2[ndxuse] - x1[ndxuse]) * \
+        lambdad[ndxuse] = 2. / 9. / np.pi / np.sqrt(x2[ndxuse] - x1[ndxuse]) * \
                           (((1. - x2[ndxuse]) * (2. * x2[ndxuse] + x1[ndxuse] - 3.) -
                             3. * x3[ndxuse] * (x2[ndxuse] - 2.)) * Kk + (x2[ndxuse] -
                                                                          x1[ndxuse]) * (z[ndxuse] ** 2 + 7. * p ** 2 - 4.) * Ek -
                            3. * x3[ndxuse] / x1[ndxuse] * ellpic_bulirsch(n, q))
 
-        notused4 = where(((z[notusedyet] <= 0.5 + abs(p - 0.5)) |
-                          (z[notusedyet] >= 1. + p)) & ((p <= 0.5) |
+        notused4 = np.where(((z[notusedyet] <= 0.5 + abs(p - 0.5)) |
+                             (z[notusedyet] >= 1. + p)) & ((p <= 0.5) |
                                                         (z[notusedyet] <= abs(1. - p)) |
                                                         (z[notusedyet] >= p)))
-        if size(notused4) == 0:
+        if np.size(notused4) == 0:
             muo1 = 1. - ((1. - u1 - 2. * u2) * lambdae + (u1 + 2. * u2) * (lambdad + 2. / 3. *
                                                                            (p > z)) + u2 * etad) / omega
             mu0 = 1. - lambdae
@@ -219,8 +229,8 @@ def occultquad(z, u1, u2, p0):
 
     # Case 3, 4, 9, 10 - planet completely inside star
     if p < 1.:
-        inside = where(z[notusedyet] <= (1. - p))  #, complement=notused5)
-        if size(inside) != 0:
+        inside = np.where(z[notusedyet] <= (1. - p))  #, complement=notused5)
+        if np.size(inside) != 0:
             ndxuse = notusedyet[inside]
 
             # eta_2
@@ -230,15 +240,15 @@ def occultquad(z, u1, u2, p0):
             lambdae[ndxuse] = p ** 2
 
             # Case 4 - edge of planet hits edge of star
-            edge = where(z[ndxuse] == 1. - p)  #, complement=notused6)
-            if size(edge[0]) != 0:
+            edge = np.where(z[ndxuse] == 1. - p)  #, complement=notused6)
+            if np.size(edge[0]) != 0:
                 # lambda_5
-                lambdad[ndxuse[edge]] = 2. / 3. / pi * arccos(1. - 2. * p) - \
-                                        4. / 9. / pi * sqrt(p * (1. - p)) * (3. + 2. * p - 8. * p ** 2)
+                lambdad[ndxuse[edge]] = 2. / 3. / np.pi * np.arccos(1. - 2. * p) - \
+                                        4. / 9. / np.pi * np.sqrt(p * (1. - p)) * (3. + 2. * p - 8. * p ** 2)
                 if p > 0.5:
                     lambdad[ndxuse[edge]] -= 2. / 3.
-                notused6 = where(z[ndxuse] != 1. - p)
-                if size(notused6) == 0:
+                notused6 = np.where(z[ndxuse] != 1. - p)
+                if np.size(notused6) == 0:
                     muo1 = 1. - ((1. - u1 - 2. * u2) * lambdae + (u1 + 2. * u2) *
                                  (lambdad + 2. / 3. * (p > z)) + u2 * etad) / omega
                     mu0 = 1. - lambdae
@@ -246,33 +256,34 @@ def occultquad(z, u1, u2, p0):
                 ndxuse = ndxuse[notused6[0]]
 
             # Case 10 - origin of planet hits origin of star
-            origin = where(z[ndxuse] == 0)  #, complement=notused7)
-            if size(origin) != 0:
+            origin = np.where(z[ndxuse] == 0)  #, complement=notused7)
+            if np.size(origin) != 0:
                 # lambda_6
                 lambdad[ndxuse[origin]] = -2. / 3. * (1. - p ** 2) ** 1.5
-                notused7 = where(z[ndxuse] != 0)
-                if size(notused7) == 0:
+                notused7 = np.where(z[ndxuse] != 0)
+                if np.size(notused7) == 0:
                     muo1 = 1. - ((1. - u1 - 2. * u2) * lambdae + (u1 + 2. * u2) *
                                  (lambdad + 2. / 3. * (p > z)) + u2 * etad) / omega
                     mu0 = 1. - lambdae
                     return [muo1, mu0]
                 ndxuse = ndxuse[notused7[0]]
 
-            q = sqrt((x2[ndxuse] - x1[ndxuse]) / (1. - x1[ndxuse]))
+            q = np.sqrt((x2[ndxuse] - x1[ndxuse]) / (1. - x1[ndxuse]))
             n = x2[ndxuse] / x1[ndxuse] - 1.
             Ek, Kk = ellke(q)
 
             # Case 3, Case 9 - anywhere in between
             # lambda_2
-            lambdad[ndxuse] = 2. / 9. / pi / sqrt(1. - x1[ndxuse]) * \
-                              ((1. - 5. * z[ndxuse] ** 2 + p ** 2 + x3[ndxuse] ** 2) * Kk +
+            lambdad[ndxuse] = (2. / 9. / np.pi / np.sqrt(1. - x1[ndxuse]) *
+                               ((1. - 5. * z[ndxuse] ** 2 + p ** 2 + x3[ndxuse] ** 2) * Kk +
                                (1. - x1[ndxuse]) * (z[ndxuse] ** 2 + 7. * p ** 2 - 4.) * Ek -
-                               3. * x3[ndxuse] / x1[ndxuse] * ellpic_bulirsch(n, q))
+                               3. * x3[ndxuse] / x1[ndxuse] * ellpic_bulirsch(n, q)))
 
         # if there are still unused elements, there's a bug in the code
         # (please report it)
-        notused5 = where(z[notusedyet] > (1. - p))
-        if notused5[0] != 0:
+        notused5 = np.where(z[notusedyet] > (1. - p))
+        if notused5[0].size > 0:  # Uli
+        # if notused5[0] != 0:
             print("ERROR: the following values of z didn't fit into a case:")
             return [-1, -1]
 
@@ -283,4 +294,12 @@ def occultquad(z, u1, u2, p0):
     return None
 
 
-print(occultquad([0.001], 0.3, 0.3, [0.1]))
+# Example call to occultquad()
+# distances = np.linspace(0, 2, 100)  # Distance values from 0 to 2
+distances = np.array([0.0, 0.02, 0.099, 0.2, 0.8, 1.0, 1.1])
+limb_darkening1 = 0.6  # Limb-darkening coefficient 1
+limb_darkening2 = 0.0  # Limb-darkening coefficient 2
+planet_radius = 0.099  # Planet radius in stellar radii
+
+result = occultquad(distances, limb_darkening1, limb_darkening2, planet_radius)
+print(result)
