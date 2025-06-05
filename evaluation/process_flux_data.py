275faaa2608c477df893a4626355b8fb59c41e25
lichtgestalter <uli.scheuss@gmail.com>
2025-06-05 23:13:52 +0200

    0.4.3   Fitting TOI-4504

--- evaluation/process_flux_data.py
+++ evaluation/process_flux_data.py
@@ -83,12 +83,15 @@ def scale_flux(flux_df, factor):
 
 
 def main():
+    path = '../research/star_systems/TOI-4504/lightkurve/'
+
+
     half_sample_duration = 0.4  # time interval we are interested in: before and after time of transit transit
     half_ignore_duration = 0.07  # time interval we want to exclude: between T1 and T4
     t88d = 2460695.535  # TT of TOI4504-d in sector 88
     t89d = 2460736.635  # TT of TOI4504-d in sector 89
 
-    csv_88_89 = '../research/star_systems/TOI-4504/lightkurve/TOI4504_88+89_all.csv'  # contains all flux data from sectors 88 and 89
+    csv_88_89 = path + 'TOI4504_88+89_all.csv'  # contains all flux data from sectors 88 and 89
     flux_df = csv2df(csv_88_89)  # usually contains df['time'], df['flux'], df['flux_err']
     flux_df.time += 2457000  # offset in TESS data
 
@@ -110,10 +113,10 @@ def main():
 
     # append t89d_df to t88d_df
     t88_89_df = pd.concat([t88d_df, t89d_df], ignore_index=True)
-    t88_89_df.to_csv('../research/star_systems/TOI-4504/lightkurve/TOI4504_88+89_reduced_normalized.csv', sep=',', decimal='.', index=False)
+    t88_89_df.to_csv(path + 'TOI4504_88+89_reduced_normalized.csv', sep=',', decimal='.', index=False)
 
-    plot_this(t88_89_df.time, [t88_89_df.flux], ["flux"], plot_file="88_89_rn.png")
-    plot_this(t88_89_df.time, [t88_89_df.flux], ["flux"], left=t88d - half_sample_duration, right=t88d + half_sample_duration, plot_file="88_rn.png")
-    plot_this(t88_89_df.time, [t88_89_df.flux], ["flux"], left=t89d - half_sample_duration, right=t89d + half_sample_duration, plot_file="89_rn.png")
+    plot_this(t88_89_df.time, [t88_89_df.flux], ["flux"], plot_file=path+"88_89_rn.png")
+    plot_this(t88_89_df.time, [t88_89_df.flux], ["flux"], left=t88d - half_sample_duration, right=t88d + half_sample_duration, plot_file=path+"88/88_rn.png")
+    plot_this(t88_89_df.time, [t88_89_df.flux], ["flux"], left=t89d - half_sample_duration, right=t89d + half_sample_duration, plot_file=path+"89/89_rn.png")
 
 main()
