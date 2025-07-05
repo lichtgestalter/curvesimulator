85ae62755060b8c613ea1d413276a908b8e2e39e
lichtgestalter <uli.scheuss@gmail.com>
2025-07-05 22:53:55 +0200

    0.5.3   MCMC

--- src/curvesimulator/cs_rebound.py
+++ src/curvesimulator/cs_rebound.py
@@ -4,16 +4,14 @@ class CurveSimRebound:
     def __init__(self, rebound_sim):
         self.sim = rebound_sim
         self.energy = rebound_sim.energy()
-        self.total_momentum = self.calc_total_momentum()
+        # self.total_momentum = self.calc_total_momentum()
         self.total_angular_momentum = self.calc_total_angular_momentum()
         self.center_of_mass_position = self.calc_center_of_mass_position()
 
     def sim_check_deltas(self, newer):
         energy_change = (newer.energy - self.energy) / self.energy
-        ntm = newer.total_momentum
-        stm = self.total_momentum
-        total_momentum_change = np.linalg.norm((ntm - stm) / stm)
         # total_momentum_change = np.linalg.norm((newer.total_momentum - self.total_momentum) / self.total_momentum)
+        total_momentum_change = 0
         total_angular_momentum_change = np.linalg.norm((newer.total_angular_momentum - self.total_angular_momentum) / self.total_angular_momentum)
         center_of_mass_position_change = np.linalg.norm(newer.center_of_mass_position - self.center_of_mass_position)
         print(f"{energy_change=:.2e}  {total_momentum_change=:.2e}  {total_angular_momentum_change=:.2e}  {center_of_mass_position_change=:.2e}  ")
--- src/curvesimulator/curvesim.py
+++ src/curvesimulator/curvesim.py
@@ -7,9 +7,24 @@ from .cs_flux_data import *
 
 
 def curvesim(config_file=""):
-    mcmc_debug = False
+    mode = "rebound_debug"
+    # mode = "mcmc"
+    # mode = "video"
     flux_debug = False
-    if mcmc_debug:
+    if mode == "rebound_debug":
+        parameters = CurveSimParameters(config_file)  # Read program parameters from config file.
+        bodies = CurveSimBodies(parameters)  # Read physical bodies from config file and initialize them, calculate their state vectors and generate their patches for the animation
+        lightcurve, rebound_sim = bodies.calc_physics(parameters)  # Calculate all body positions and the resulting lightcurve
+        print(f"star at last transit: {bodies[0].positions[99474]}")
+        print(f"   b at last transit: {bodies[1].positions[99474]}")
+        print(f"   c at last transit: {bodies[2].positions[99474]}")
+        print(f"   d at last transit: {bodies[3].positions[99474]}")
+        area, impact = bodies[0].eclipsed_by(bodies[2], 99474, parameters)
+        # area = 0 if area is None else area
+        # impact = 0 if impact is None else impact
+        print(f"{area=:.6e}  {impact=:.6f}")
+        return parameters, bodies, None, None
+    if mode == "mcmc":
         parameters = CurveSimParameters(config_file)  # Read program parameters from config file.
         bodies = CurveSimBodies(parameters)  # Read physical bodies from config file and initialize them, calculate their state vectors and generate their patches for the animation
         flux, mask = try_corresponding_flux(parameters)
@@ -18,7 +33,7 @@ def curvesim(config_file=""):
             debug_flux(parameters, flux, mask, lightcurve)
         mcmc(mask, bodies, flux, parameters)
         return parameters, bodies, None, None
-    else:
+    if mode == "video":
         parameters = CurveSimParameters(config_file)  # Read program parameters from config file.
         bodies = CurveSimBodies(parameters)  # Read physical bodies from config file and initialize them, calculate their state vectors and generate their patches for the animation
         lightcurve, rebound_sim = bodies.calc_physics(parameters)  # Calculate all body positions and the resulting lightcurve
