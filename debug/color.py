56c47d09280e4f8b79e9f3c278b479432dc140da
lichtgestalter <uli.scheuss@gmail.com>
2025-08-15 09:43:48 +0200

    0.5.6 Fitting TOI-4504

--- debug/color.py
+++ debug/color.py
@@ -1,17 +1,19 @@
 from colorama import Fore, Style, Back
 
-print(f"{Fore.RED}This is red text{Style.RESET_ALL}")
-print(f"{Style.DIM}{Fore.RED}This is red text{Style.RESET_ALL}")
-print(f"{Style.NORMAL}{Fore.RED}This is red text{Style.RESET_ALL}")
-print(f"{Style.BRIGHT}{Fore.RED}This is red text{Style.RESET_ALL}")
-print(f"{Fore.RED}{Back.WHITE}This is red text{Style.RESET_ALL}")
-print(f"{Fore.GREEN}This is green text{Style.RESET_ALL}")
-print(f"{Fore.BLUE}This is blue text{Style.RESET_ALL}")
-
-
 # Foreground Colors: Fore.
 # Background Colors: Back.
 # Possible Colors: BLACK, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE, RESET
 
 # Text Styles: Style.
     # DIM, NORMAL, BRIGHT, RESET_ALL
+
+print(f"{Style.DIM}{Fore.RED}This is dimmed red text{Style.RESET_ALL}")
+print(f"{Style.NORMAL}{Fore.RED}This is normal red text{Style.RESET_ALL}")
+print(f"{Style.BRIGHT}{Fore.RED}This is bright red text{Style.RESET_ALL}")
+print(f"{Fore.RED}{Back.WHITE}This is red text with white background{Style.RESET_ALL}")
+print()
+colors = ["BLACK", "RED", "GREEN", "YELLOW", "BLUE", "MAGENTA", "CYAN", "WHITE"]
+color_names = Fore.BLACK, Fore.RED, Fore.GREEN, Fore.YELLOW, Fore.BLUE, Fore.MAGENTA, Fore.CYAN, Fore.WHITE
+
+for c, cn in zip(colors, color_names):
+    print(f"{cn}This is {c}{Style.RESET_ALL}")
