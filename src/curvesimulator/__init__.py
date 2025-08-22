c1047916583cfb4e7b5d31f53670648a183ba4fc
lichtgestalter <uli.scheuss@gmail.com>
2025-08-23 00:26:46 +0200

    0.5.6 Fitting TOI-4504

--- src/curvesimulator/__init__.py
+++ src/curvesimulator/__init__.py
@@ -3,10 +3,10 @@ from .curvesim import CurveSimulator
 from .cs_animation import CurveSimAnimation
 from .cs_bodies import CurveSimBodies
 from .cs_body import CurveSimBody
-# from .cs_flux_data import *
+from .cs_flux_data import csv2df
 from .cs_lightcurve import CurveSimLightcurve
 from .cs_mcmc import CurveSimMCMC
-from .cs_parameters import CurveSimParameters
+from .cs_parameters import CurveSimParameters, FittingParameter
 from .cs_physics import CurveSimPhysics
 from .cs_rebound import CurveSimRebound
 from .cs_results import CurveSimResults
