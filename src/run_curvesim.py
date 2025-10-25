5f394aa364ab68a3796a45f26e1b789dc119c414
lichtgestalter <uli.scheuss@gmail.com>
2025-10-25 22:07:40 +0200

    0.5.6 Fitting TOI-4504

--- src/run_curvesim.py
+++ src/run_curvesim.py
@@ -26,7 +26,7 @@ def main():
     # curvesimulation = CurveSimulator(config_file="../configurations/Sim/TOI-4504_SIM_X045.ini")
     # curvesimulation = CurveSimulator(config_file="../configurations/TOI4504/debug.ini")
     # curvesimulation = CurveSimulator(config_file="../configurations/TOI4504/TOI-4504_X050.ini")
-    curvesimulation = CurveSimulatorMulti(config_file="../configurations/TOI4504/TOI-4504_X050.ini")
+    curvesimulation = CurveSimulatorMulti(config_file="../configurations/TOI4504/TOI-4504_X053.ini")
     print(curvesimulation)
 
 
