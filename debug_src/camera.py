f3adabd662cc765d63df9eb3b377c3f6fde34fb7
lichtgestalter <uli.scheuss@gmail.com>
2026-02-18 21:44:45 +0100

    0.5.7 Automated Flux Data Download

--- debug_src/camera.py
+++ debug_src/camera.py
@@ -3,5 +3,5 @@ camera = cv2.VideoCapture(0)
 while True:
     ret, frame = camera.read()
     print(ret)
-    cv2.imshow("Fenstername", frame)
+    cv2.imshow("some window name", frame)
     cv2.waitKey(0)
