0443d2629895fe67cb2d9fb960cdad43be09de72
lichtgestalter <uli.scheuss@gmail.com>
2026-03-10 23:02:35 +0100

    0.5.8 TOI-4504 Videos

--- src/run_curvesim.py
+++ src/run_curvesim.py
@@ -18,12 +18,19 @@
 #     main()
 
 
+# Compatibility shim: some older libraries expect numpy.in1d which may be removed in newer NumPy versions.
+# Provide a safe alias to numpy.isin before importing other packages (e.g., astropy/lightkurve) that use it.
+# import numpy as np
+# if not hasattr(np, 'in1d'):
+#     np.in1d = np.isin
+
+
 # Current version for developers
 from curvesimulator import CurveSimulator
 
 def main():
     # curvesimulation = CurveSimulator(config_file="../configurations/TOI4504/debug.ini")
-    curvesimulation = CurveSimulator(config_file="../configurations/TOI4504/TOI-4504_V003.ini")
+    curvesimulation = CurveSimulator(config_file="../configurations/TOI4504/TOI-4504_V006.ini")
     print(curvesimulation)
 
 
