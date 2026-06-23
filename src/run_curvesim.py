cc0e1d02d90331e742efebcc1aa3f4f493887097
lichtgestalter <uli.scheuss@gmail.com>
2026-06-23 16:49:00 +0200

    0.5.8 TOI-4504 Videos

--- src/run_curvesim.py
+++ src/run_curvesim.py
@@ -26,8 +26,8 @@ def main():
     # curvesimulation = CurveSimulator(config_file="../results/T200/TOI-4504_T200.ini")
     # curvesimulation = CurveSimulator(config_file="../results/Michaela1stPaper/TOI-4504_Vitkova_1st_paper.ini")
     # curvesimulation = CurveSimulator(config_file="../results/Trifon_22.03.26/Trifon_22.03.26.ini")
-    # curvesimulation = CurveSimulator(config_file="../results/Almenara_AppendixA1/Almenara_AppendixA1.ini")
-    curvesimulation = CurveSimulator(config_file="../results/Almenara/Almenara.ini")
+    curvesimulation = CurveSimulator(config_file="../results/Almenara_AppendixA1/Almenara_AppendixA1.ini")
+    # curvesimulation = CurveSimulator(config_file="../results/Almenara/Almenara.ini")
 
 
     print(curvesimulation)
