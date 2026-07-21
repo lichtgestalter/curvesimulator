04cdac315507eb273f0ca804ff51a848bbb17621
lichtgestalter <uli.scheuss@gmail.com>
2026-07-21 12:59:55 +0200

    0.6 preparing new release (Example2: lmfit)

--- doc/examples/2/run_example.py
+++ doc/examples/2/run_example.py
@@ -1,3 +1,6 @@
 from curvesimulator import CurveSimulator
 
 curvesimulation = CurveSimulator(config_file="config.ini")
+
+
+# Next: lmfit multiprocessing runtime error
--- src/run_curvesim.py
+++ src/run_curvesim.py
@@ -22,13 +22,15 @@
 from curvesimulator import CurveSimulator
 
 def main():
+    # curvesimulation = CurveSimulator(config_file="../../curvesimulator.internal/results/PosterScreenshot/TOI-4504_PosterScreenshot.ini")
+
     # curvesimulation = CurveSimulator(config_file="../../curvesimulator.internal/results/debug/debug.ini")
     # curvesimulation = CurveSimulator(config_file="../../curvesimulator.internal/results/T200/TOI-4504_T200.ini")
-    curvesimulation = CurveSimulator(config_file="../../curvesimulator.internal/results/PosterScreenshot/TOI-4504_PosterScreenshot.ini")
     # curvesimulation = CurveSimulator(config_file="../../curvesimulator.internal/results/Michaela1stPaper/TOI-4504_Vitkova_1st_paper.ini")
     # curvesimulation = CurveSimulator(config_file="../../curvesimulator.internal/results/Trifon_22.03.26/Trifon_22.03.26.ini")
     # curvesimulation = CurveSimulator(config_file="../../curvesimulator.internal/results/Almenara_AppendixA1/Almenara_AppendixA1.ini")
     # curvesimulation = CurveSimulator(config_file="../../curvesimulator.internal/results/Almenara/Almenara.ini")
+    curvesimulation = CurveSimulator(config_file="../../curvesimulator.internal/configurations/TOI4504/astrocentric/TOI-4504_lmfit_retry.ini")
 
     print(curvesimulation)
 
