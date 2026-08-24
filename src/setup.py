a3cc49318be8e544d470c0ce8bf9690e0eafb168
lichtgestalter <uli.scheuss@gmail.com>
2026-08-24 13:22:22 +0200

    0.6.3.2 Package compatibilities

--- src/setup.py
+++ src/setup.py
@@ -2,32 +2,32 @@ import os
 from setuptools import setup, find_packages
 
 # Read project info from ../README.md and put it on pypi.org
-current_directory = os.path.abspath(os.path.dirname(__file__))  # Get the directory containing this file
-current_directory = os.path.dirname(current_directory)  # remove the src directory from the path
-readme_path = os.path.join(current_directory, 'README.md')  # Construct the path to the README file
+current_directory = os.path.abspath(os.path.dirname(__file__))  # get the directory containing this file
+current_directory = os.path.dirname(current_directory)          # remove the src directory from the path
+readme_path = os.path.join(current_directory, 'README.md')      # construct the path to the README file
 
 with open(readme_path, "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
     name="curvesimulator",
-    version="0.6.3.1",
+    version="0.6.3.2",
     packages=find_packages(),
     install_requires=[
-        # List your dependencies here
-        "colorama",           # prints colored text in console
-        "configparser",       # reads config file
-        "corner",             # generates corner plot for mcmc results
-        "emcee",              # Markov Chain Monte Carlo
-        # "lightkurve",         # TESS light-curve download/processing
-        "lmfit",              # local minimization fits
-        "matplotlib==3.10.0",  # plots
-        "numpy",              # numerical (vector) operations
-        # "numpy>=1.5,<2.3",    example showing how to constrain versions
-        "pandas",             # tabular flux / TT / RV data
-        "rebound>=5.1.1",     # N-body integration
-        # "rebound @ https://github.com/hannorein/rebound/archive/refs/tags/5.1.0.tar.gz",  # PyPI sdist is incomplete for Windows/Python 3.13; GitHub release archive includes missing assembly source
-        "scipy",              # statistics and optimization helpers
+        "colorama",             # prints colored text in console
+        "configparser",         # reads config file
+        "corner",               # generates corner plot for mcmc results
+        "emcee",                # Markov Chain Monte Carlo
+        "lmfit",                # local minimization fits
+        "matplotlib",           # plots
+        "numpy",                # numerical (vector) operations
+        "pandas",               # tabular flux / TT / RV data
+        "rebound>=5.1.1",       # N-body integration
+        "scipy",                # statistics and optimization helpers
+        "tqdm",                 # emcee progression bar
+        # "matplotlib==3.10.0",   # plots
+        # "numpy>=1.5,<2.3",      # example showing how to constrain versions
+        # "lightkurve",           # TESS light-curve download/processing
     ],
     author="Uli Scheuss",
     description="CurveSimulator is a n-body library for orbital parameter determination and visualization of exoplanet systems.",
